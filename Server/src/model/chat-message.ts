abd1a1065062a4c7ebdcb2e94f43f579609430dd
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-26 12:48:09 -0500

    Finished the server setup

---
+++ Server/src/model/chat-message.ts
@@ -0,0 +1,10 @@
+import { Message } from "./message";
+import { User } from "./user";
+
+export class ChatMessage extends Message
+{
+    constructor(from: User, content: string)
+    {
+        super(from, content);
+    }
+}
