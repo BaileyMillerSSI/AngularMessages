e805fc4402d8c0933dc389db92ef527caeb13308
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-26 12:52:11 -0500

    Added the base models to the index port

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,4 +1,8 @@
 import { ChatServer } from './chat-server';
 
+import { User } from "./model/user";
+import { Message } from "./model/message";
+import { ChatMessage } from "./model/chat-message";
+
 let app = new ChatServer().getApp();
-export { app };
+export { app, User };
